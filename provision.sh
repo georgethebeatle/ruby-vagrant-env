236631e290adce4f5ecf83c491b557d894568f1b
Georgi Sabev <georgi.sabev@sap.com>
2014-09-11 11:05:32 +0300

    Install ruby 1.9.3 on a fresh vm

---
+++ provision.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y make git vim
+git clone https://github.com/sstephenson/ruby-build.git
+./ruby-build/install.sh
+ruby-build "$(ruby-build --definitions | grep 1.9.3 | tail -1)" /home/vagrant/local/ruby-$(ruby-build --definitions | grep 1.9.3 | tail -1)
+echo "export PATH=/home/vagrant/local/ruby-1.9.3-p547/bin/:\$PATH" >> /home/vagrant/.bashrc
